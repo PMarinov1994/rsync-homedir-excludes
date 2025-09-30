a46ac57174315139635f12469bca5569039d1b5a
PMarinov1994 <plamen_944@outlook.com>
2025-10-01 00:23:40 +0300

    Update shell scripts

--- restore.sh
+++ restore.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 rsync -aPvh root@192.168.1.109:/srv/dev-disk-by-uuid-1de46d53-850b-4909-ad65-46cccb8d6f2f/rsync/$USER/ /home/$USER/
+
+# yay -S $(cat /home/$USER/yay-install.txt)
+# yay -S --needed - < /home/$USER/yay-install.txt
